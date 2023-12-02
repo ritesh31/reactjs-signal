2c43f46b7bffe112d47e32419d9c67c3daa824eb
ritesh3105 <ritesh.d@amazatic.com>
2023-12-02 17:37:50 +0530

    All about effect() of preact/signals-react.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
-import { useState } from "react";
 import "./App.css";
-import ChildOne from "./components/ChildOne";
-import { useSignal } from "@preact/signals-react";
+import { useSignal, effect } from "@preact/signals-react";
 
 function App() {
-  const [countState, setCountState] = useState(0);
   const countSignal = useSignal(0);
-  const [temp, setTemp] = useState("Temp");
+
+  effect(() => {
+    console.log("Inside effect");
+  });
+
   return (
     <div className="container">
       <h3>App component</h3>
       <div className="inner-container">
-        <div className="box">
-          <p>
-            <strong>State Counter: </strong>
-            {countState}
-          </p>
-          <button onClick={() => setCountState(countState + 1)}>
-            State Count++
-          </button>
-        </div>
-
         <div className="box">
           <p>
             <strong>Signal Counter: </strong>
@@ -28,33 +19,24 @@ function App() {
           </p>
           <button onClick={() => countSignal.value++}>Signal Count++</button>
         </div>
-
-        <div className="box">
-          <p>
-            <strong>Temp: </strong>
-            {temp}
-          </p>
-          <button onClick={() => setTemp("Temp Changed")}>Change</button>
-        </div>
       </div>
-      <ChildOne countState={countState} countSignal={countSignal} />
+      {/* <ChildOne countState={countState} countSignal={countSignal} /> */}
     </div>
   );
 }
 
 export default App;
 
-// Using useState()
-// 1. If we are using useState() with the help of memo() All child component re-rendered
-//    because we are using parent data inside child components
-//    but we need to add memo() for all child components
+// Using effect()
+// We don't have to pass dependencies array like the useEffect hook. 
+// It'll automatically detect dependencies and call effect only when we accessing signal value.
+// Ex. 
+//  effect will not call bcoz we have no used any signal value
+//  effect(() => {
+//    console.log("Inside effect");
+//  });
 
-// 2. If we are changing state using useState() hook which are not passing to child component
-//    then whole App component re-rendered
-
-// Using useSignal()
-// 1. If we are using usSignal() only value will we re-rendered not any Child component will re-rendered
-//    also no need of memo()
-// 2. Bcoz useSignal() passing object of signal variable
-//    and object contains value so React will actually checking signal object not its value
-//    that's why React noot re-rendered component
+//  effect will call bcoz we have no used any signal value
+//  effect(() => {
+//    console.log("Inside effect", countSignal.value);
+//  });
